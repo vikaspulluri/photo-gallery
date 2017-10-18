9bda1dbb4478940dd03a5436f04eccf42eccfcd6
vikaspulluri <vikasiiitn@gmail.com>
2017-10-18 22:20:31 +0530

    First commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database Constants
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER')   ? null : define("DB_USER", "vikas_gallery");
+defined('DB_PASS')   ? null : define("DB_PASS", "Vikmysql@2017");
+defined('DB_NAME')   ? null : define("DB_NAME", "photo_gallery");
+
+?>
